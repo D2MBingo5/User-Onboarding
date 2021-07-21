e5f6b3528c07edffa4e4074c98a8a1f4efcbd0a2
Leu Family <romanianfobjr@gmail.com>
2021-07-21 16:20:23 -0500

    MVP complete besides compiler error from axios api pull, will ask during evening qna

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,13 +1,107 @@
 import logo from './logo.svg';
 import './App.css';
+
 import Form from './Form'
+import schema from './formSchema'
+import User from './User'
+
+import axios from 'axios'
+import { reach } from 'yup'
+import React, { useState, useEffect } from 'react';
+
+// initial states
+const initialFormValues = {
+  name: '',
+  email: '',
+  pw: '',
+  tos: false,
+}
+const initialFormErrors = {
+  name: '',
+  email: '',
+  pw: '',
+  tos: false,
+}
+const initialUsers = []
+const initialDisabled = true
 
 function App() {
+  // states
+  const [users, setUsers] = useState(initialUsers)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const getUsers = () => {
+    axios.get('https://reqres.in/api/users')
+      .then(res => {
+        setUsers(res.data)
+      })
+      .catch(err => {console.log(err)})
+  }
+
+  const postNewUser = newUser => {
+    axios.post('https://reqres.in/api/users', newUser)
+      .then(res => {
+        setUsers([res.data, ...users])
+      })
+      .catch(err => {console.log(err)})
+      .finally(() => {setFormValues(initialFormValues)})
+  }
+
+  const validate = (name, value) => {
+    reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: ''}))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
+  }
+
+  const inputChange = (name, value) => {
+    validate(name, value)
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const formSubmit = () => {
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      pw: formValues.pw.trim(),
+      tos: formValues.tos
+    }
+    postNewUser(newUser)
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  useEffect(() => {
+    schema.isValid(formValues).then(valid => setDisabled(!valid))
+  }, [formValues])
+
   return (
     <div className="App">
       <header><h1>User Onboarding Form</h1></header>
 
-      <Form />
+      <Form 
+        values={formValues}
+        submit={formSubmit}
+        change={inputChange}
+        disabled={disabled}
+        errors={formErrors}
+      />
+
+      {
+        users.map(user => {
+          return (
+            <User key={user.id} details={user} />
+          )
+        })
+      }
+
     </div>
   );
 }
