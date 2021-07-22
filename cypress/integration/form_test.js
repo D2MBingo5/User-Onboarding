f6eb958632cda0c421ed0168aad77706af9a7926
Leu Family <romanianfobjr@gmail.com>
2021-07-22 15:31:10 -0500

    sanity check testing properly

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,1 +1,13 @@
+import { iteratee } from "lodash"
+
 // test folder
+describe('User Onboarding Form App', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/') // Assumes port 3000 is being used
+    })
+
+    it('sanity check', () => {
+        expect(1+2).to.equal(3)
+        expect(2+2).not.to.equal(5)
+    })
+})
